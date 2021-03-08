748253a2ff2f0af26673408102d2ff1c931bc415
Amrut-17 <68114007+Amrut-17@users.noreply.github.com>
2021-03-08 16:27:28 +0530

    Weird Algorithm
    Easy

---
+++ WeirdAlgorithm.java
@@ -0,0 +1,21 @@
+//package CSES;
+
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long n = s.nextLong();
+        while( n != 1){
+            System.out.print(n + " ");
+            if (n%2 ==0){
+                n = n/2;
+            }
+            else {
+                n = (n*3)+1;
+            }
+        }
+        System.out.print(n);
+
+    }
+}
